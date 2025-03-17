315406a48e096d73783caf8e742be0208378cf48
TSheep <mggodmine@gmail.com>
2025-03-17 12:03:55 -0300

    IMC v1.0
    Versão já está funcionando mas vou fazer ajustes com o tempo

--- script/calculo_imc.js
+++ script/calculo_imc.js
@@ -1,16 +1,23 @@
 const click = document.getElementById("clica")
+const resp = document.getElementById("resp")
 let altura = document.getElementById("altura")
 let peso = document.getElementById("peso")
 
+// Calculo de IMC
 
 click.onclick = function(){
-    const pesonum = Number(peso.value)
-    const alturanum = Number(altura.value)
+    let pesonum = Number(peso.value)
+    let alturanum = Number(altura.value)
+    
 
     if (alturanum <= 0 && pesonum <= 0){
         window.alert('[Erro] Talvez o peso ou a altura não tenha sido colocado de maneira correta.')
     } else {
-        let IMC = pesonum / (alturanum**2)
-        window.alert(`IMC é: (${IMC.toFixed(2)})`)
+       let IMC = pesonum / (alturanum**2)
+       let text = document.getElementById('text')
+       text.innerHTML = ''
+       text.innerHTML = (`Seu IMC é ${IMC.toFixed(2)}`)
+       resp.style.display = 'block'
+       text.appendChild()
     }
 }
